007461f061c147a99b51bf1241b6c33b2b709a45
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-06-23 12:24:39 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<strong>About</strong> <br>
+Lesson from Digital Crafts bootcamp.
+
+<strong>About the exercises</strong> <br>
+
+
+<strong>Lessons learned</strong> <br>
+React.createElement() <br>
+ReactDOM.render()<br>
---
+++ bookstore/index.js
@@ -0,0 +1,62 @@
+// Step 1. describe what you want the page to look like
+/* <div>
+    <h1>Bookstore</h1>
+    <ul class="book-list">
+        <li>Books are coming soon</li>
+    </ul>
+    <footer>Copyright 2018</footer>
+</div>   */
+
+// <h1>Bookstore</h1>
+// $('<h1>', { text: 'Bookstore'})
+// $('.react-root').append(element);
+
+// creates an object that includes the info you gave it
+// the 2nd argument can be either a class, id or href
+// the 3rd argument is the text that goes inside the element
+
+
+// let title = h('h1', null, 'Bookstore'); 
+// let copyright = h('footer', null, 'Copyright 2018'); 
+// let bookRow = h('footer', null, 'Books are coming soon!'); 
+// let booklist = h('ul', { className: 'book-list' }, bookRow);
+// let container = h('div', null, [
+//     title, 
+//     booklist,
+//     copyright,
+// ]); 
+
+const books = [
+    { title: 'A Tale of Two Cities', author: 'Charles Dickens' },
+    { title: 'War and Peach', author: 'Leo Tolstory' },
+    { title: 'A Count of Monte Cristo', author: 'Alexandre Dumas' },
+];
+
+let h = React.createElement;
+
+// take the list of books and generate a list item
+// an array of list item of virtual dom
+// let bookRows = books.map(book =>
+//     h('li', null, book.title + ' by ' + book.author)
+// );
+
+
+// this is a virtual dom
+let container = h('div', null, [
+    h('h1', null, 'Bookstore'), 
+    h('ul', { className: 'book-list' }, 
+        books.map(book =>
+            h('li', null, book.title + ' by ' + book.author)
+        )
+    ),
+    h('footer', null, 'Copyright 2018'),
+]); 
+
+
+// document.querySelector('.react-root').appendChild(element);
+// 1st argument is what element to render
+// 2nd arguement is where to put it
+ReactDOM.render(
+    container, 
+    document.querySelector('.react-root')
+);
