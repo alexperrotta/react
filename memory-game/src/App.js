865e3cb1d89df1c68ee19351d031f10ffd55eba0
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-06-29 20:17:41 -0400

    changes to part 2

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
-import MemoryCard from './MemoryCard.js';
 import './App.css';
+
+import MemoryCard from './MemoryCard.js';
 
 class App extends Component {
   render() {
+
+    var row1 = [];
+    var row2 = [];
+    var row3 = [];
+    var row4 = [];
+    for (var i=0; i<4; i++) {
+      row1.push(<MemoryCard />);
+      row2.push(<MemoryCard />);
+      row3.push(<MemoryCard />);
+      row4.push(<MemoryCard />);
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Memory Game</h1>
-          <h3 className="App-subtitle">Match cards to win</h3>
-          <div className="MemoryCard" />
+          <p className="App-subtitle">Match cards to win</p>
         </header>
+
+      <div>
+        {row1}
+      </div>
+      <div>
+        {row2}
+      </div>
+      <div>
+        {row3}
+      </div>
+      <div>
+        {row4}
+      </div>
+
       </div>
 
     );
--- memory-game/src/MemoryCard.js
+++ memory-game/src/MemoryCard.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
-import './App.css';
 import './MemoryCard.css';
 
 class MemoryCard extends Component {
-  render() {
-    return (
-      <div className="MemoryCard">
-          <img className="logo" src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" />
-      </div>
-    );
+  
+  constructor() {
+    super();
+    this.state = {isFlipped: false};
   }
+
+  clickHandler() {
+    this.setState({
+      isFlipped: !this.state.isFlipped
+    });
+  }
+  
+  render() {
+    let memoryCardInnerClass = 'memoryCardInner';
+    if(isFlipped == true) {
+      memoryCardInnerClass.append('flipped');
+    } else {
+      
+    }
+    return (
+      <div className="MemoryCard" onClick={ this.clickHandler }>
+        <div className="MemoryCardInner">
+          
+          <div className="MemoryCardBack"><img src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png"/></div>
+
+          <div className="MemoryCardFront">∆</div>
+          
+        </div>
+      </div>
+    )
+  }
+
 }
 
 export default MemoryCard;
